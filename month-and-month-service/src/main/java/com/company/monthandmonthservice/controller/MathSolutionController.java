f0163b04c68ead5d7543c33b4e7621bb6dff8a66
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-10 21:40:12 -0700

    Additional post path for subtract, multiply and divide developed and work as per the requirments.

--- month-and-month-service/src/main/java/com/company/monthandmonthservice/controller/MathSolutionController.java
+++ month-and-month-service/src/main/java/com/company/monthandmonthservice/controller/MathSolutionController.java
@@ -6,12 +6,37 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MathSolutionController {
-
+//     Addition
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public MathSolution findSumOfTwoIntegers(@RequestBody MathSolution mathSolution){
+    public MathSolution SumOfTwoNumbers(@RequestBody MathSolution mathSolution){
         mathSolution.setOperation("add");
         mathSolution.setAnswer();
         return mathSolution;
     }
+    //     Subtraction
+    @RequestMapping(value = "/subtract", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public MathSolution DifferenceOfTwoNumbers(@RequestBody MathSolution mathSolution){
+        mathSolution.setOperation("subtract");
+        mathSolution.setAnswer();
+        return mathSolution;
+    }
+    //     Multiply
+    @RequestMapping(value = "/multiply", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public MathSolution multiplicationOfTwoNumbers(@RequestBody MathSolution mathSolution){
+        mathSolution.setOperation("multiply");
+        mathSolution.setAnswer();
+        return mathSolution;
+    }
+    //     Division
+    @RequestMapping(value = "/divide", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public MathSolution divisionOfTwoNumbers(@RequestBody MathSolution mathSolution){
+        mathSolution.setOperation("divide");
+        mathSolution.setAnswer();
+        return mathSolution;
+    }
+
 }
